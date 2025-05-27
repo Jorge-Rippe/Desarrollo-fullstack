1b19a4a11916be660b712416e90c493d5987c2b8
jorge rippe <jorgerippe.com>
2025-05-26 21:36:45 -0500

    feat: add new file operaciones

---
+++ operaciones.js
@@ -0,0 +1,8 @@
+// operaciones aritmeticas en js
+let a = 3, b = 2;
+let z = a + b;
+console.log("resultado de la suma: " + z);
+z = a - b;
+console.log("resultado de la resta: " + z);
+z = a * b;
+console.log("resultado de la multiplicacion: " + z);
